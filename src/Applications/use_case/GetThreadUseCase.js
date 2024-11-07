ecbc0ea0fc6a571106ca808eeed6d453c9ff7541
galaparadi <gala_koyin@yahoo.co.id>
2024-11-07 20:40:44 +0700

    [UPDATE] GetThreadUseCase & test to use new repository method

--- src/Applications/use_case/GetThreadUseCase.js
+++ src/Applications/use_case/GetThreadUseCase.js
@@ -2,35 +2,17 @@ const Thread = require('../../Domains/threads/entities/Thread');
 
 class GetThreadUseCase {
   constructor({
-    threadRepository, commentRepository, replyRepository, commentLikeRepository,
+    threadRepository, commentRepository,
   }) {
     this._threadRepository = threadRepository;
     this._commentRepository = commentRepository;
-    this._replyRepository = replyRepository;
-    this._commentLikeRepository = commentLikeRepository;
   }
 
   async execute(useCasePayload) {
     const { threadId } = useCasePayload;
     const thread = new Thread(await this._threadRepository.getThread(threadId));
-    const comments = await this._commentRepository.getCommentsFromThread(threadId);
-
-    /* eslint no-await-in-loop: 0 */
-    /* eslint no-restricted-syntax: 0 */
-    for (const comment of comments) {
-      try {
-        const replies = await this._replyRepository.getRepliesFromComment(comment.id);
-        replies.forEach((reply) => {
-          comment.replies.push(reply);
-          comment.replies.sort((replyA, replyB) => replyA.date - replyB.date);
-        });
-      } catch (error) {
-        // DO nothing. Just skip and continue.
-      }
-      comment.likeCount = (await this._commentLikeRepository.getLikeCount(comment.id)).count;
-      thread.comments.push(comment);
-      thread.comments.sort((commentA, commentB) => commentA.date - commentB.date);
-    }
+    const comments = await this._commentRepository.getCommentsAndReplyFromThread(threadId);
+    thread.comments = comments.MapToArray().sort((a, b) => a.date - b.date);
 
     return thread;
   }
